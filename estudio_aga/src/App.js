ffa4ce4d87cedac28417f886e0a551fe8ca5cc72
Lucas Pereira <lucas.pereira@ccc.ufcg.edu.br>
2024-02-05 15:06:41 -0300

    Criação do projeto

---
+++ estudio_aga/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Container } from './Header/headerStyle';
+
+function App() {
+  return (
+    <Container></Container>
+  );
+}
+
+export default App;
---
+++ estudio_aga/src/Header/headerStyle.js
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 200vh;
+  display: flex;
+  background-color: blue;
+`;
